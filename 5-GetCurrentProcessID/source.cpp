07f79b64a714c716b4227609ce4e5e9410f68687
CyberChimeraUSA <cyberchimerausa@gmail.com>
2018-05-13 08:11:28 -0700

    Source Updates

---
+++ 5-GetCurrentProcessID/source.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <windows.h>
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+	cout << GetCurrentProcessId() << endl;
+
+	return 0;
+}
+
